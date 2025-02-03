832a85e6036081f507dc2183345774f561665d83
Dre-Walt <deiwalters25@gmail.com>
2025-02-03 18:32:59 -0400

    updated lab

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-import click, sys
+import sys, click
 from models import db, User
 from app import app
 from sqlalchemy.exc import IntegrityError
@@ -7,8 +7,9 @@ from sqlalchemy.exc import IntegrityError
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
   db.drop_all()
-  db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  db.session.add(bob)
+  db.session.commit()
   print(bob)
   print('database intialized')
